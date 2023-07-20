c9c343ff108a15becc452010435b75efa254f7cd
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-07-19 20:47:08 -0400

    basic address generator

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module go-ton-generator
 
 go 1.19
+
+require github.com/xssnick/tonutils-go v1.7.4
+
+require (
+	github.com/sigurn/crc16 v0.0.0-20211026045750-20ab5afb07e3 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/xssnick/tonutils-go/ton"
+	"github.com/xssnick/tonutils-go/ton/wallet"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	// init ton client
+	api := ton.NewAPIClient(nil)
+
+	// generate N wallets
+	now := time.Now()
+	count := 10
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+	goodCnt := 0
+	for i := 0; i < count; i++ {
+		go func() {
+			defer wg.Done()
+			// random sleep from 0.1 to 1 sec
+			time.Sleep(time.Duration(rand.Intn(1000)+100) * time.Millisecond)
+			words := wallet.NewSeed()
+
+			w, _ := wallet.FromSeed(api, words, wallet.V4R2)
+			addr := w.Address().String()
+
+			// filter out addresses with - and _
+			if !strings.Contains(addr, "-") && !strings.Contains(addr, "_") {
+				log.Println("good wallet: ", addr)
+				goodCnt++
+			}
+		}()
+	}
+	wg.Wait()
+	log.Printf("%d/%d good wallets generated in %s\n", goodCnt, count, time.Since(now))
 }
